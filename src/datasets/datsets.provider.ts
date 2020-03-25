467b19ccdf55be66aae3246b031f72c6b3cd6381
JiangWeixian <saber_wei@163.com>
2020-03-25 12:04:18 +0800

    🐛 (datasets): connect

--- src/datasets/datsets.provider.ts
+++ src/datasets/datsets.provider.ts
@@ -5,6 +5,6 @@ export const databaseProviders: FactoryProvider[] = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://jiangweixian:saberwei520@ds019688.mlab.com:19688/draws'),
+      mongoose.connect('mongodb://jiangweixian:3AcRzoEI4TuXMcGQM1B8@ds019688.mlab.com:19688/draws'),
   },
 ];
